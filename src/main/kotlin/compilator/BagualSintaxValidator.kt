09fe6eceda8bdeaed24ff88f1cfdf02863a1d8eb
Arthur Lima de Souza <souza.arthur@cwi.com.br>
2021-06-27 17:56:20 -0300

    add sintax validation

--- src/main/kotlin/compilator/BagualSintaxValidator.kt
+++ src/main/kotlin/compilator/BagualSintaxValidator.kt
@@ -4,7 +4,7 @@ enum class BagualSintaxValidator(val sintax: String, val regex: Regex, val error
 
     MAIN_FUNCTION_OPEN("", Regex("^VAMO DALE main\\(\\) TCHÊ"), "Main function não declarada da forma correta"),
     MAIN_FUNCTION_CLOSE("", Regex("FEITORIA$"), "Main function não foi fechada corretamente"),
-    FOR_METHOD("TE PASSO O LAÇO", Regex("TE PASSO O LAÇO\\([a-z] in \\d+\\.\\.\\d+\\) TCHÊ"), "Laço \"TE PASSO O\" não definido da maneira correta"),
+    FOR_METHOD("TE PASSO O LAÇO", Regex("TE PASSO O LAÇO\\([a-z] ATÉ \\d+\\.\\.\\d+\\) TCHÊ"), "Laço \"TE PASSO O\" não definido da maneira correta"),
     IF_CONDITION("SE PÁ", Regex("SE PÁ\\(\\w+ [==|<=|>=|<|>]+ .*\\) TCHÊ"), "Condição \"SE PÁ\" não declarada da forma correta"),
     PRINT_METHOD("TÁ NA MÃO", Regex("TÁ NA MÃO\\(.*\\)"), "Método \"TÁ NA MÃO\" declarado incorretamente");
 
